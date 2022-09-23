4e939c634acd402ece6a4bb6cbdfc6f128a22736
souzafcharles <souzach@outlook.com>
2022-09-22 21:58:29 -0300

    Mechanic Service Tests

--- MechanicalWorkshopTest/MechanicTest.java
+++ MechanicalWorkshopTest/MechanicTest.java
@@ -1,2 +1,10 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@DisplayName("Mechanic Service Tests")
 public class MechanicTest {
 }
